9d06acd3673b6b3b152475423b18bdb1abfaf6ad
Bastian <bastianxpfeifer@gmail.com>
2025-05-26 13:11:17 +0200

    first version ready

--- R/topkgraphs.R
+++ R/topkgraphs.R
@@ -2,8 +2,45 @@ library(TopKLists)
 library(igraph)
 
 
-adjmatrix1 = omics_binary[[1]]
-adjmatrix2 = omics_binary[[2]]
+topkgraphs <- function(views, walk_depth=20, n_iter=20){
+
+ RES = list()
+ n_nodes = nrow(views[[1]])
+
+ cat("The graph has", n_nodes, "Nodes. \n")
+ for(xx in 1:n_nodes){
+
+    RES[[xx]] = topkgraphs_walk(views, 
+                            start_node=xx, 
+                            walk_depth=walk_depth, 
+                            n_iter=n_iter)
+ }
+
+#return(RES)
+
+# Convert to distance matrix
+DIST = matrix(NaN, length(RES), length(RES))
+
+for (xx in 1:length(RES)){
+
+    DIST[xx, as.numeric(RES[[xx]]$TopK[,1])] = RES[[xx]]$Scores[,1]
+
+}
+
+DIST = DIST/max(DIST, na.rm=TRUE)
+DIST[is.na(DIST)] = 1
+
+return(DIST)
+
+}
+
+
+
+topkgraphs_walk <- function(views, start_node=1, walk_depth=20, n_iter=20){
+
+adjmatrix1 = views[[1]]
+adjmatrix2 = views[[2]]
+
 
 # Convert to graph object
 graph1 = graph_from_adjacency_matrix(
@@ -25,10 +62,6 @@ graph2 = graph_from_adjacency_matrix(
   add.rownames = NA
 )
 
-
-walk_depth = 10 
-n_iter = 20 
-start_node = 1
 
 WALKS1 = matrix(NaN, walk_depth + 1, n_iter)
 
@@ -68,9 +101,20 @@ for(xx in 1:n_iter){
 # Concatenate the walks from the multi-view graphs
 WALKS = cbind(WALKS1, WALKS2)
 
-res1 = apply(WALKS, 2, table)
-res2 = sapply(res1, sort, decreasing=TRUE)
-n_nodes = max(sapply(res2, length))
+#print(WALKS)
+
+res1 = apply(WALKS, 2, table, simplify=FALSE)
+
+#print(res1)
+
+res2 = sapply(res1, sort, decreasing=TRUE, simplify=FALSE)
+
+#print(res2)
+
+n_nodes = max(as.numeric(unlist(sapply(res2, names))))
+
+#print(n_nodes)
+#print(ncol(WALKS))
 
 rankMatrix = matrix(NaN, n_nodes, ncol(WALKS))
 
@@ -80,27 +124,61 @@ for (xx in 1:ncol(WALKS)){
     rankMatrix[1:length(res2[[xx]]),xx] = names(res2[[xx]])
 
 }
+#print(rankMatrix)
 
 # check missings
 values = unique(rankMatrix)
 na_ids = which(rankMatrix=="NaN", arr.ind=TRUE)
+na_cols = na_ids[,2]
 
-for(xx in 1:nrow(na_ids)){
+for(xx in 1:length(na_cols)){
 
-   repl_val =  setdiff(values, rankMatrix[,na_ids[xx,2]])
+   repl_val =  setdiff(values, rankMatrix[, na_cols[xx]])
+   if(length(repl_val)==0){next}
+   repl_ids = which(rankMatrix[,na_cols[xx]]=="NaN")
    #print(repl_val)
-   repl_ids = which(rankMatrix[,na_ids[xx,2]]=="NaN")
-   rankMatrix[repl_ids, na_ids[xx,2]] = sample(repl_val, length(repl_val), 
+   #print("start")
+   #print(na_cols[xx])
+   #print(rankMatrix)
+   rankMatrix[repl_ids, na_cols[xx]] = sample(repl_val, length(repl_val), 
                                                 replace=FALSE) 
+   #xx = xx + length(repl_ids) - 1
+   #print("done")
 
 }
 
+### check for remaining NaNs
+
+#print(rankMatrix)
+
+na_ids = which(rankMatrix=="NaN", arr.ind=TRUE)
+na_rows = unique(na_ids[,1])
+
+#print(na_rows)
+
+if(length(na_rows)!=0){
+    rankMatrix = rankMatrix[-na_rows,]
+}
+
+#print(rankMatrix)
+
 # Check the correctness of that! @FIXME
 # BORDA ---------------------------------------- #
-print("BORDA")
+#print("BORDA")
 rankMatrix2 <- apply(rankMatrix,2, function(x) match(1:length(x),x))
+
+#print(rankMatrix2)
+
 rankMatrix2 <- matrix(as.character(rankMatrix2), dim(rankMatrix2)[1],dim(rankMatrix2)[2])
+
+#print(rankMatrix2)
+
+rankMatrix2 = rankMatrix
+
 IN          <- lapply(seq_len(ncol(rankMatrix2)), function(i) rankMatrix2[,i])
+
+#print(IN)
+
 borda.res   <- Borda(IN)
 # l2norm
 TKSrank_l2norm   <- borda.res$TopK$l2norm
@@ -116,3 +194,7 @@ TKSrank_geomean   <- borda.res$TopK$geo.mean
 TKSrank_geomean   <- match(1:length(TKSrank_geomean),as.numeric(TKSrank_geomean))
 #print(length(TKSrank))
 # ---------------------------------------------- #
+
+return(borda.res)
+
+}
